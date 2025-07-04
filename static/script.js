3a0e04fd7f556a24932856e6a43191b6addf0a56
Doug Aquino <rpa.dougaquino@gmail.com>
2025-07-04 18:14:50 -0300

    fix break pause

--- static/script.js
+++ static/script.js
@@ -1,41 +1,67 @@
 
 // function helper ----------------------------------------
-function updateDisplay(){
+function updateTimer(){
     timer_min.textContent = String(min_remain).padStart(2, "0");
     timer_sec.textContent = String(secs_remain).padStart(2, "0");
+}
+
+function updateMessageNColor(color=0){
+    switch (color) {
+        case 1:
+            span_msg.textContent = "Running Pomodoro."
+            span_msg.style.color = "rgb(11, 102, 35)";
+            main_body.style.background = "rgba(11, 102, 35, 0.4)";
+            break;
+    
+        case 2:
+            span_msg.textContent = "Pomodoro Paused."
+            span_msg.style.color = "rgb(208, 3, 3)"
+            main_body.style.background = "rgba(208, 3, 3, 0.4)";
+            break;
+        
+        case 3:
+            span_msg.textContent = "Break Time! Good job! :)";
+            span_msg.style.color = "rgb(255, 195, 11)"
+            main_body.style.background = "rgba(255, 195, 11, 0.4)";
+            break;
+
+        default:
+            span_msg.textContent = "Waiting for Start."
+            span_msg.style.color = "rgb(255, 195, 11)"
+            main_body.style.background = "rgba(238, 238, 238, 0.4)";
+            break;
+    }
 }
 
 
 // functions handlers ------------------------------------
 function toggleStart(){
     if (is_running === false){
-        btn_control.innerText = "Pause";
+        btn_control.textContent = "Pause";
         btn_reset.removeAttribute("disabled");
-        span_msg.innerText = "Running Pomodoro."
-        span_msg.style.cssText = "color: rgb(11, 102, 35)";
-        main_body.style.cssText = "background: rgba(11, 102, 35, 0.4);";
+        if (break_time !== true){
+            updateMessageNColor(1);
+        } else{
+            updateMessageNColor(3);
+        }
         is_running = true;
     } else {
-        btn_control.innerText = "Start";
-        span_msg.innerText = "Pomodoro Paused."
-        span_msg.style.cssText = "color: rgb(208, 3, 3)"
-        main_body.style.cssText = "background: rgba(208, 3, 3, 0.4);";
+        btn_control.textContent = "Start";
+        updateMessageNColor(2);
         is_running = false;
     }
 }
 
 
 function reset(){
-    btn_control.innerText = "Start";
+    btn_control.textContent = "Start";
     btn_reset.setAttribute("disabled", true);
     min_remain = 30;
     secs_remain = 0;
     is_running = false;
-    break_time = true;
-    updateDisplay();
-    span_msg.innerText = "Waiting for Start."
-    span_msg.style.cssText = "color: rgb(255, 195, 11)"
-    main_body.style.cssText = "background: rgba(238, 238, 238, 0.4);";
+    break_time = false;
+    updateTimer();
+    updateMessageNColor(0);
 }
 
 
@@ -43,15 +69,13 @@ function clocksTick() {
     if (!is_running) return;
 
     if (min_remain === 0 && secs_remain === 0) {
+        break_time = !break_time;
         if (break_time === true){
             min_remain = 5;
             secs_remain = 0;
             is_running = true;
-            span_msg.innerText = "Break Time! Good job! :)";
-            span_msg.style.cssText = "color: rgb(255, 195, 11)"
-            main_body.style.cssText = "background: rgba(255, 195, 11, 0.4);";
-            break_time = false;
-            updateDisplay();
+            updateTimer();
+            updateMessageNColor(3);
             return;
         }
 
@@ -68,7 +92,7 @@ function clocksTick() {
         secs_remain--;
     }
 
-    updateDisplay();
+    updateTimer();
 }
 
 
@@ -83,14 +107,14 @@ const main_body = document.getElementById("main-body");
 
 // state -----------------------------------------------------
 let is_running = false;
-let break_time = true;
+let break_time = false;
 let min_remain = 30;
 let secs_remain = 0;
 
 // start code
 btn_reset.setAttribute("disabled", true);
 reset();
-updateDisplay();
+updateTimer();
 
 btn_control.addEventListener("click", toggleStart);
 btn_reset.addEventListener("click", reset);
